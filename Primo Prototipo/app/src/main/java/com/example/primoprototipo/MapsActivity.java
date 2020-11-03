71ba998d9ca49c90801cd66a3c085a3577b75fe8
Lotto98 <46002750+Lotto98@users.noreply.github.com>
2020-11-03 22:36:47 +0100

    bho

--- Primo Prototipo/app/src/main/java/com/example/primoprototipo/MapsActivity.java
+++ Primo Prototipo/app/src/main/java/com/example/primoprototipo/MapsActivity.java
@@ -34,6 +34,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
