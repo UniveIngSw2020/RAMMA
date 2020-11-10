9e0593668467f3d3d841928e0bdd8a07f71fac07
Lotto98 <46002750+Lotto98@users.noreply.github.com>
2020-11-10 21:14:17 +0100

    Piccole modifiche QR commerciante

--- Primo_prototipo/app/src/main/java/com/example/rent_scio1/QRGeneratorTrader.java
+++ Primo_prototipo/app/src/main/java/com/example/rent_scio1/QRGeneratorTrader.java
@@ -2,21 +2,46 @@ package com.example.rent_scio1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
 
 public class QRGeneratorTrader extends AppCompatActivity {
 
     private static final String ToQR="QR_code_creation";
+
+    Bitmap bitmap;
+    ImageView qrImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q_r_generator_trader);
 
-        //ID univoco del veicolo
-        final int ID=getIntent().getIntExtra(ToQR,0);
+        qrImage=findViewById(R.id.QR_code);
 
-        
+        //ID univoco del veicolo
+        final int ID_veicolo=getIntent().getIntExtra(ToQR,0);
+
+        //Utente commerciante usare la classe statica UserClient.getID()
+
+        QRGEncoder qrgEncoder = new QRGEncoder(, null, QRGContents.Type.TEXT,500);
+
+        try {
+            // Getting QR-Code as Bitmap
+            bitmap = qrgEncoder.encodeAsBitmap();
+
+            // Setting Bitmap to ImageView
+            qrImage.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            Log.v("ciao", e.toString());
+        }
     }
 }
