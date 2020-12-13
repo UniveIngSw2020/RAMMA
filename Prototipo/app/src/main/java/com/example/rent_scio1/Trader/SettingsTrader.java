1539fedf113cf555dff0da16f4f79dbefd66a56a
Lotto98 <46002750+Lotto98@users.noreply.github.com>
2020-12-13 15:14:46 +0100

    Aggiunto scheletro per impostazioni comm

---
+++ Prototipo/app/src/main/java/com/example/rent_scio1/Trader/SettingsTrader.java
@@ -0,0 +1,14 @@
+package com.example.rent_scio1.Trader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsTrader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_trader);
+    }
+}
