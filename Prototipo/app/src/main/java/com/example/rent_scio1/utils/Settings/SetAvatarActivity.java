591e0d54ce5bc1d04dafcffde91d1366c4286c78
andreachinellato98 <71335281+andreachinellato98@users.noreply.github.com>
2020-12-20 15:09:20 +0100

    Modifiche interfaccia grafica impostazioni settaggio avatar cliente.
    Cambiata l'interfaccia grafica, aggiunto bottone nella toolbar per aggiungere un'immagine dal dispositivo.
    Aggiunto bottone per eliminare avatr e utilizzare quello di default.
    TODO: codice per l'eliminazione e l'utilizzazione dell'avatar di default.

--- Prototipo/app/src/main/java/com/example/rent_scio1/utils/Settings/SetAvatarActivity.java
+++ Prototipo/app/src/main/java/com/example/rent_scio1/utils/Settings/SetAvatarActivity.java
@@ -2,34 +2,36 @@ package com.example.rent_scio1.utils.Settings;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.rent_scio1.Client.SettingsCustomer;
 import com.example.rent_scio1.R;
+import com.example.rent_scio1.Trader.NewVehicleActivityTrader;
 import com.example.rent_scio1.utils.UserClient;
+import com.example.rent_scio1.utils.Vehicle;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Objects;
 
 public class SetAvatarActivity extends AppCompatActivity {
 
@@ -43,14 +45,13 @@ public class SetAvatarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_set_avatar);
 
         imageView=(ImageView)findViewById(R.id.avatar);
+        initViews();
 
-        //setto il comportamento del bottone carica
-        Button buttonLoadImage = findViewById(R.id.buttonLoadPicture);
-        buttonLoadImage.setOnClickListener(arg0 -> {
+        /*TODO: BOTTONE PER ELIMINA AVATAR.*/
+        Button delete_avatar = findViewById(R.id.elimina_avatar);
+        delete_avatar.setOnClickListener(v -> {
 
-            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-            photoPickerIntent.setType("image/*");
-            startActivityForResult(photoPickerIntent, RESULT_LOAD_IMAGE);
+            /*INSERIRE CODICE PER ELIMINA AVATAR*/
         });
 
         //setto il comportamento del bottone conferma
@@ -75,6 +76,31 @@ public class SetAvatarActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void initViews(){
+        Toolbar toolbar_settings = findViewById(R.id.toolbar_settings_avatar);
+        setSupportActionBar(toolbar_settings);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_settings_avatar_client, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_one_image) {
+
+            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+            photoPickerIntent.setType("image/*");
+            startActivityForResult(photoPickerIntent, RESULT_LOAD_IMAGE);
+            return true;
+        } else
+            return super.onOptionsItemSelected(item);
     }
 
     @Override
